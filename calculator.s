6dd5e8f3a8cccfd54d8118ac52bae2887dbc22c4
Aizen Murza <islam.muratazaev@gmail.com>
2025-02-25 13:51:38 -0500

    Format UI messages

--- calculator.s
+++ calculator.s
@@ -2,14 +2,17 @@
 buffer: .skip 4
 
 .section .data
-start_menu: .asciz "Calculator App\n"
 
+@ UI messages
+menu: .asciz "Calculator App\n1-Add\n2-Subtract\n3-Multiply\n4-Divide\n5-Exit\n"
 operation_prompt: .asciz "Enter Choice > "
 operand1_prompt: .asciz "Enter Operand 1 > "
 operand2_prompt: .asciz "Enter Operand 2 > "
+exit_msg: .asciz "Application Exit!\n"
 
+@ Formats for input
 str_fmt: .asciz "%s"
-int_fmt: .asciz "%d" @ Format string for integer input
+int_fmt: .asciz "%d"
 sum_fmt: .asciz "%d+%d=%d\n"
 
 .section .text
@@ -20,18 +23,23 @@ sum_fmt: .asciz "%d+%d=%d\n"
 _start:
 	@ Print start menu
 	ldr r0, =str_fmt
-	ldr r1, =start_menu
+	ldr r1, =menu
 	bl printf
-input_variables:
-	@ Take user input to calculate
-	@ Stores r8 = operation, r9 = operand1, r10 = operand2
-
+input_operation:
 	@ Take user input for operation
+	@ store r8 = selected operation
 	ldr r0, =str_fmt
 	ldr r1, =operation_prompt
 	bl printf
 	bl input
 	ldr r8, [r1]
+
+	@ check if user chose to exit
+	cmp r8, #5
+	beq end
+input_operands:
+	@ Take user input for operands
+	@ Stores r9 = operand1, r10 = operand2
 
 	@ Take user input for operand 1
 	ldr r0, =str_fmt
@@ -53,6 +61,12 @@ output:
 	mov r3, r10
 	bl printf
 end:
+	@ print exit message
+	ldr r0, =str_fmt
+	ldr r1, =exit_msg
+	bl printf
+
+	@ sys exit call
 	mov r7, #1
 	mov r0, #0
 	svc #0
@@ -61,6 +75,7 @@ end:
 
 input:
 	@ reads from stdin and loads it to buffer
+	@ stores the memory location of input in r1
 	push {lr}
 
 	ldr r0, =int_fmt
